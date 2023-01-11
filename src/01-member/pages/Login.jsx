f108c6837839bdcb46df518d2750bf4a6df71a23
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-01-11 14:42:12 +0800

    edit styles of login form

--- src/01-member/pages/Login.jsx
+++ src/01-member/pages/Login.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Login() {
+
   return (
-    <div className='max-w-2xl mx-auto'>
-        <div className='bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8'>
+    <div className='max-w-2xl mx-auto pt-4'>
+        <div className='bg-white shadow-md border border-gray-200 rounded-lg mx-auto max-w-sm p-4 sm:p-6 lg:p-8'>
             <form className='space-y-6'>
             <h3 class="text-xl font-medium text-gray-900 dark:text-white">歡迎回來～～～</h3>
             <div className='w-full'>
-                <label htmlFor='account' className='text-sm font-medium text-gray-900 mb-2 block'>帳號</label>
+                <label htmlhtmlFor='account' className='text-sm font-medium text-gray-900 mb-2 block'>帳號</label>
                 <input className='bg-gray-50 border-gray-300 border text-sm rounded-lg w-full p-2.5' name='account' id='account'
                 type='text' placeholder='請輸入您的帳號'></input>
             </div>
             <div className='w-full'>
-                <label htmlFor='password' className='text-sm font-medium text-gray-900 mb-2 block'>密碼</label>
+                <label htmlhtmlFor='password' className='text-sm font-medium text-gray-900 mb-2 block'>密碼</label>
                 <input className='bg-gray-50 border-gray-300 border text-sm rounded-lg w-full p-2.5' name='password' id='password'
                 type='password' placeholder='••••••••'></input>
             </div>
-                
-                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600">Login to your account</button>
+            <div className='w-full flex justify-between'>
+                <div class="flex items-start">
+                    <div class="flex items-center h-5">
+					    <input id="remember" aria-describedby="remember" type="checkbox" class="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required=""/>
+                    </div>
+					<div class="text-sm ml-3">
+						<label htmlFor="remember" class="font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+					</div>
+                </div>
+                <div class="flex items-start mr-5">
+                    <div class="flex items-center h-5 ">
+					    <Link to='/forget-pw' className='font-medium text-blue-700 '>Lost Password?</Link>
+                    </div>
+                </div>
+            </div>
+            
+                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                Login to your account
+                </button>
+                <div className='flex itmes-start'>
+                    <p className='font-medium text-gray-600 mr-3'>Not registered?</p>
+                    <Link className='font-medium text-blue-700' to='/register'>Create account</Link>
+                </div>
             </form>
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 
 function Navbar() {
     const navigate = useNavigate()
+    const [login, setLogin] = useState(false)
 
     return (
-    <header className='flex w-full items-center justify-between border-b-2 border-gray-200 bg-white p-3 navbar_container'>
+    <header className='flex w-full items-center justify-between border-b-2 border-gray-200 bg-white p-2 mb-3'>
         <div className="flex items-center space-x-2">
             <button type="button" className="text-3xl">
             <i className="fa-sharp fa-solid fa-bars"></i>
@@ -15,17 +16,28 @@ function Navbar() {
             </div>
         </div>
         <div className="w-48 sm:w-96 flex items-center justify-between p-2">
-                    <Link to="/#" className="transition hover:text-blue-600 ">Login</Link>
+                    <Link to="/login" className="transition hover:text-blue-600 ">Login</Link>
                     <Link to="/#" className="transition hover:text-blue-600">Products</Link>
                 </div>
         <div className='right-2 mt-1 w-48 divide-y divide-gray-200 rounded-md'>
-            <div className="flex items-center space-x-2 p-2">
+        {login? 
+        <div className="flex items-center space-x-2 p-2">
                         <img src="https://plchldr.co/i/40x40?bg=111111" alt="plchldr.co" className="h-9 w-9 rounded-full" />
                         <div className="font-medium">斑比姊姊</div>
                         <div>
                             <Link to='/login'>Logout</Link>
                         </div>
             </div>
+        :
+        <div className="flex items-center space-x-2 p-2">
+        <img src="https://plchldr.co/i/40x40?bg=111111" alt="plchldr.co" className="h-9 w-9 rounded-full" />
+                        <div className="font-medium">請登入</div>
+                        <div>
+                            <Link to='/login'>Login</Link>
+                        </div>
+        </div>    
+        }
+            
         </div>
     </header>
   )
